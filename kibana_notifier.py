3a8d20376c231ffeac01bdafc7b09f6791dfb3ac
Motea Marius <marius.motea@gmail.com>
2020-11-18 13:39:13 +0200

    Create kibana_notifier.py

---
+++ kibana_notifier.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import docker
+import json
+import smtplib
+
+
+smtp_server = "10.x.x.x"
+recipients = "mariusmotea@xxxxxxx.com"
+subject = "Alert from Kibana"
+sender_address = "elastic@xxxxxxx.com"
+
+def sendMail(TEXT):
+    """this is some test documentation in the function"""
+    message = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sender_address, ", ".join(recipients), subject, TEXT)
+    # Send the mail
+    server = smtplib.SMTP(smtp_server)
+    server.sendmail(sender_address, recipients, message)
+    server.quit()
+
+
+client = docker.from_env()
+container = client.containers.get('kib01')
+
+for line in container.logs(stream=True, tail=10):
+    record = json.loads(line)
+    if record["type"] == "log" and "error" in record["tags"]:
+        sendMail(record["message"]
